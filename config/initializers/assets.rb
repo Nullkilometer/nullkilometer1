a9f574863c9e8c9fc24046627c28d3377da3489e
Vitalija Korkuc <tankiaitaskuota@gmail.com>
2020-03-06 10:53:05 +0200

    fixup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.1'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += ['.js', '.css', '*.scss']
+#Rails.application.config.assets.precompile += ['.js', '.css', '*.scss']
 
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'html')
 Rails.application.config.assets.register_mime_type('text/html', '.html')
