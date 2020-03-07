b8943a268ad28e4809f5c3b8ecd4671f76d4e52c
TankiaiTaskuota <tankiaitaskuota@gmail.com>
2020-03-07 10:49:58 +0200

    assets fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Nullkilometer::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [:de]
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
